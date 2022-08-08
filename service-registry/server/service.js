e05866c0ae95f6b104593e412573b5644925d9c5
Amir Mustafa <amirengg15@gmail.com>
2022-08-08 14:18:27 +0530

    feat: service registry - register service endpoints

--- service-registry/server/service.js
+++ service-registry/server/service.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const service = express();
 // const ServiceRegistry = require('./ServiceRegistry');
@@ -6,12 +6,29 @@ const service = express();
 module.exports = (config) => {
   const log = config.log();
   // Add a request logging middleware in development mode
-  if (service.get('env') === 'development') {
+  if (service.get("env") === "development") {
     service.use((req, res, next) => {
       log.debug(`${req.method}: ${req.url}`);
       return next();
     });
   }
+
+  // Registering a service
+  service.put(
+    "/register/:servicename/:serviceversion/:serviceport",
+    (req, res, next) => {
+      return next("not implemented.");
+    }
+  );
+  service.delete(
+    "/register/:servicename/:serviceversion/:serviceport",
+    (req, res, next) => {
+      return next("not implemented.");
+    }
+  );
+  service.get("/find/:servicename/:serviceversion", (req, res, next) => {
+    return next("not implemented.");
+  });
 
   // eslint-disable-next-line no-unused-vars
   service.use((error, req, res, next) => {
