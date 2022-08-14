3f8822f59a2c0ca6e16be52299fd640efddb6378
Amir Mustafa <amirengg15@gmail.com>
2022-08-14 21:18:07 +0530

    feat: adding skeleton of speaker service - routes

--- speakers-service/server/service.js
+++ speakers-service/server/service.js
@@ -1,16 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 const service = express();
 
 module.exports = (config) => {
   const log = config.log();
   // Add a request logging middleware in development mode
-  if (service.get('env') === 'development') {
+  if (service.get("env") === "development") {
     service.use((req, res, next) => {
       log.debug(`${req.method}: ${req.url}`);
       return next();
     });
   }
+
+  // Adding all routes for speakers micro-service
+  service.get("/list", (req, res, next) => {
+    return next("not implemented");
+  });
+
+  service.get("/list-short", (req, res, next) => {
+    return next("not implemented");
+  });
+
+  service.get("/names", (req, res, next) => {
+    return next("not implemented");
+  });
+
+  service.get("/speaker/:shortname", (req, res, next) => {
+    return next("not implemented");
+  });
+
+  service.get("/artwork/:shortname", (req, res, next) => {
+    return next("not implemented");
+  });
 
   // eslint-disable-next-line no-unused-vars
   service.use((error, req, res, next) => {
